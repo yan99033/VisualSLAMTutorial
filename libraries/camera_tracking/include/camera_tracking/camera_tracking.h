4e83ea8573bf3b2192abeb29f47821f44299822e
Shing-Yan Loo <yan99033@gmail.com>
2022-06-05 17:07:05 -0600

    Style: auto-format the code

--- libraries/camera_tracking/include/camera_tracking/camera_tracking.h
+++ libraries/camera_tracking/include/camera_tracking/camera_tracking.h
@@ -2,13 +2,11 @@
 #define __LOAD_FROM_FOLDER_H__
 
 namespace vslam_libs {
-namespace camera_tracking {
+  namespace camera_tracking {
 
-class CameraTracking 
-{
-};
+    class CameraTracking {};
 
-}  // namespace camera_tracking
+  }  // namespace camera_tracking
 }  // namespace vslam_libs
 
 #endif  // __LOAD_FROM_FOLDER_H__
--- libraries/image_loader/include/image_loader/load_from_folder.h
+++ libraries/image_loader/include/image_loader/load_from_folder.h
@@ -2,13 +2,11 @@
 #define __LOAD_FROM_FOLDER_H__
 
 namespace vslam_libs {
-namespace image_loader {
+  namespace image_loader {
 
-class LoadFromFolder
-{
-};
+    class LoadFromFolder {};
 
-}  // namespace image_loader
+  }  // namespace image_loader
 }  // namespace vslam_libs
 
 #endif  // __LOAD_FROM_FOLDER_H__
