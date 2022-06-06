ec09fe7506531288e787d6e857104f3bbca650dd
Shing-Yan Loo <yan99033@gmail.com>
2022-06-05 18:07:40 -0600

    Chore: add a postcondition to ensure there are image files in a folder

--- libraries/image_loader/include/image_loader/load_from_folder.h
+++ libraries/image_loader/include/image_loader/load_from_folder.h
@@ -1,19 +1,26 @@
 #ifndef __LOAD_FROM_FOLDER_H__
 #define __LOAD_FROM_FOLDER_H__
 
+#include <opencv2/core.hpp>
 #include <string>
 #include <vector>
+
 namespace vslam_libs {
   namespace image_loader {
     class LoadFromFolder {
     public:
-      LoadFromFolder(const std::string& folder);
+      LoadFromFolder(const std::string& folder, std::string ext = ".png");
+
+      cv::Mat getNextFrame();
 
     private:
-      std::vector<std::string> files;
+      unsigned int i;                  //!< pointer to the current frame in the files list
+      std::vector<std::string> files;  //!< Files in a folder
     };
 
   }  // namespace image_loader
+
+  class LoadFromFolderError : 
 }  // namespace vslam_libs
 
 #endif  // __LOAD_FROM_FOLDER_H__
--- libraries/image_loader/source/load_from_folder.cpp
+++ libraries/image_loader/source/load_from_folder.cpp
@@ -1,15 +1,25 @@
 #include "image_loader/load_from_folder.h"
 
+#include <fmt/core.h>
+
+#include <algorithm>
 #include <filesystem>
-#include <iostream>
+#include <gsl/assert>
 namespace vslam_libs {
   namespace image_loader {
 
-    LoadFromFolder::LoadFromFolder(const std::string& folder) {
+    LoadFromFolder::LoadFromFolder(const std::string& folder, std::string ext) {
       for (const auto& f : std::filesystem::directory_iterator(folder)) {
-        std::cout << "file: " << f << std::endl;
+        if (f.path().extension() == ext) {
+          files.push_back(f.path());
+        }
       }
-    }
 
+      std::sort(files.begin(), files.end());
+
+      Ensures(!files.empty());
+
+      fmt::print("Loaded {} files\n", files.size());
+    }
   }  // namespace image_loader
 }  // namespace vslam_libs
