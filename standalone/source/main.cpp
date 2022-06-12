7633a5280d909202e73747c3090a12e183a14476
Shing-Yan Loo <yan99033@gmail.com>
2022-06-12 14:21:51 -0600

    Chore: visualize images in a sequence

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,5 +1,6 @@
 #include <cxxopts.hpp>
 #include <iostream>
+#include <opencv2/highgui.hpp>
 #include <string>
 
 #include "image_loader/load_from_folder.h"
@@ -26,6 +27,13 @@ auto main(int argc, char** argv) -> int {
 
   vslam_libs::image_loader::LoadFromFolder loader(
       "/media/bryan/DATA/EuRoC_dataset/MH/MH_01/cam0/data");
+
+  while (true) {
+    cv::Mat frame = loader.getNextFrame();
+
+    cv::imshow("frame", frame);
+    cv::waitKey(1);
+  }
 
   return 0;
 }
