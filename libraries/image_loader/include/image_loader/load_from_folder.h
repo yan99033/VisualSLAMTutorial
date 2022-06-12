7b78aad8cb44c0f3f81485bc22e97fe0a3e2fbae
Shing-Yan Loo <yan99033@gmail.com>
2022-06-12 14:20:07 -0600

    Feature: load subsequent images in an image sequence

--- libraries/image_loader/include/image_loader/load_from_folder.h
+++ libraries/image_loader/include/image_loader/load_from_folder.h
@@ -2,6 +2,7 @@
 #define __LOAD_FROM_FOLDER_H__
 
 #include <opencv2/core.hpp>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -9,13 +10,18 @@ namespace vslam_libs {
   namespace image_loader {
     class LoadFromFolder {
     public:
-      LoadFromFolder(const std::string& folder, std::string ext = ".png");
+      explicit LoadFromFolder(const std::string& folder, std::string ext = ".png");
 
       cv::Mat getNextFrame();
 
     private:
       unsigned int i;                  //!< pointer to the current frame in the files list
       std::vector<std::string> files;  //!< Files in a folder
+    };
+
+    class ImageLoaderError : public std::runtime_error {
+    public:
+      ImageLoaderError(const std::string msg) : runtime_error(msg) {}
     };
 
   }  // namespace image_loader
--- libraries/image_loader/source/load_from_folder.cpp
+++ libraries/image_loader/source/load_from_folder.cpp
@@ -4,11 +4,16 @@
 
 #include <algorithm>
 #include <filesystem>
-#include <gsl/assert>
+#include <opencv2/imgcodecs.hpp>
 namespace vslam_libs {
   namespace image_loader {
 
-    LoadFromFolder::LoadFromFolder(const std::string& folder, std::string ext) {
+    LoadFromFolder::LoadFromFolder(const std::string& folder, std::string ext) : i(0) {
+      // Check if the path points to an empty file or directory
+      if (std::filesystem::is_empty(folder)) {
+        throw ImageLoaderError(fmt::format("The folder ({}) is empty.\n", folder));
+      }
+
       for (const auto& f : std::filesystem::directory_iterator(folder)) {
         if (f.path().extension() == ext) {
           files.push_back(f.path());
@@ -17,9 +22,23 @@ namespace vslam_libs {
 
       std::sort(files.begin(), files.end());
 
-      Ensures(!files.empty());
+      if (files.empty()) {
+        throw ImageLoaderError(fmt::format("The folder({}) does not have images\n", folder));
+      }
 
       fmt::print("Loaded {} files\n", files.size());
     }
+
+    cv::Mat LoadFromFolder::getNextFrame() {
+      cv::Mat frame = cv::imread(files.at(i), cv::IMREAD_COLOR);
+
+      // Keep replaying the last frame if it reaches the end of the sequence
+      if (i < files.size() - 1) {
+        i += 1;
+      }
+
+      return frame;
+    }
+
   }  // namespace image_loader
 }  // namespace vslam_libs
