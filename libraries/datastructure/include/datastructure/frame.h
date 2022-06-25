7a8494058826ff6cf2e7f217c3b9792d2b9190d7
Shing-Yan Loo <yan99033@gmail.com>
2022-06-25 16:46:24 -0600

    Chore: add feature detector to frame

--- libraries/datastructure/include/datastructure/frame.h
+++ libraries/datastructure/include/datastructure/frame.h
@@ -3,16 +3,21 @@
 
 #include <memory>
 #include <opencv2/core.hpp>
+
+#include "feature_detector/feature_detector.h"
+
 namespace vslam_libs {
   namespace datastructure {
     class Frame {
     public:
       explicit Frame(const cv::Mat& image);
-      Frame(const cv::Mat& image, const cv::Mat& cam_mat);
+      Frame(const cv::Mat& image, const cv::Mat& cam_mat,
+            feature_detector::OrbFeatureDetector* detector = nullptr);
 
     private:
       cv::Mat image;    //<! the image of the frame
       cv::Mat cam_mat;  //<! 3x3 camera matrix containing the camera intrinsics
+      feature_detector::OrbFeatureDetector* detector;  //<! ORB feature detector
     };
 
     using FramePtr = std::shared_ptr<Frame>;
--- libraries/datastructure/source/frame.cpp
+++ libraries/datastructure/source/frame.cpp
@@ -5,7 +5,12 @@ namespace vslam_libs {
   namespace datastructure {
     Frame::Frame(const cv::Mat& image) : image(image) {}
 
-    Frame::Frame(const cv::Mat& image, const cv::Mat& cam_mat) : image(image), cam_mat(cam_mat) {}
+    // Frame::Frame(const cv::Mat& image, const cv::Mat& cam_mat) : image(image), cam_mat(cam_mat)
+    // {}
+
+    Frame::Frame(const cv::Mat& image, const cv::Mat& cam_mat,
+                 feature_detector::OrbFeatureDetector* detector)
+        : image(image), cam_mat(cam_mat), detector(detector) {}
 
   }  // namespace datastructure
 }  // namespace vslam_libs
