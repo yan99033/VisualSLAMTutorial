e919343229639607839fcbbad8927bf5633f1790
Shing-Yan Loo <yan99033@gmail.com>
2022-06-25 15:30:48 -0600

    Chore: calculate camera pose between two consecutive frames

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -5,11 +5,35 @@
 #include <iostream>
 #include <memory>
 #include <nlohmann/json.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/highgui.hpp>
 #include <string>
 
 #include "datastructure/frame.h"
 #include "image_loader/load_from_folder.h"
+
+void getCorrespondences(const std::vector<cv::KeyPoint>& keypoints1,
+                        const std::vector<cv::KeyPoint>& keypoints2,
+                        const std::vector<cv::DMatch>& matches, const std::vector<char>& mask,
+                        std::vector<cv::Point2f>& points1, std::vector<cv::Point2f>& points2) {
+  // Get the matches
+  points1.clear();
+  points2.clear();
+
+  // Check if the length of matches is the same as that of mask!
+  for (size_t m = 0; m < matches.size(); m++) {
+    if (mask.empty() || mask[m]) {
+      int i1 = matches[m].queryIdx;
+      int i2 = matches[m].trainIdx;
+
+      cv::KeyPoint kp1 = keypoints1.at(i1);
+      cv::KeyPoint kp2 = keypoints2.at(i2);
+
+      points1.emplace_back(kp1.pt);
+      points2.emplace_back(kp2.pt);
+    }
+  }
+}
 
 auto main(int argc, char** argv) -> int {
   cxxopts::Options options(*argv, "Visual SLAM on a images in a folder");
@@ -52,14 +76,48 @@ auto main(int argc, char** argv) -> int {
 
     vslam_libs::image_loader::LoadFromFolder loader(image_folder);
 
+    // ORB feature detector and matcher
+    int num_features = 1000;
+    auto detector = cv::ORB::create(num_features);
+    auto matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE);
+
+    bool first_frame = true;
+    std::vector<cv::KeyPoint> keypoints1, keypoints2;
+    cv::Mat descriptors1, descriptors2;
+
     while (true) {
       cv::Mat image = loader.getNextFrame();
+
+      if (first_frame) {
+        first_frame = false;
+        detector->detectAndCompute(image, cv::noArray(), keypoints1, descriptors1);
+      }
 
       vslam_libs::datastructure::FramePtr frame
           = std::make_shared<vslam_libs::datastructure::Frame>(image, cam_mat);
 
+      detector->detectAndCompute(image, cv::noArray(), keypoints2, descriptors2);
+
+      std::vector<cv::DMatch> matches;
+      std::vector<char> matcher_mask;
+      matcher->match(descriptors1, descriptors2, matches, matcher_mask);
+
+      std::vector<cv::Point2f> points1, points2;
+      getCorrespondences(keypoints1, keypoints2, matches, matcher_mask, points1, points2);
+
+      // recovering the pose and the essential matrix
+      cv::Mat E, R, t, mask;
+      E = cv::findEssentialMat(points2, points1, cam_mat, cv::RANSAC, 0.999, 1.0, mask);
+      cv::recoverPose(E, points2, points1, cam_mat, R, t, mask);
+
+      std::cout << "pose:\n";
+      std::cout << R << "\n";
+      std::cout << t << std::endl;
       cv::imshow("image", image);
       cv::waitKey(1);
+
+      keypoints1 = keypoints2;
+      descriptors1 = descriptors2;
     }
   } catch (const std::exception& e) {
     fmt::print("Error: {}\n", e.what());
