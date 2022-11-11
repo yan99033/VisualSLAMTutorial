76375f409f57d2d4e928ddfc3cbc3ad3d312592b
Shing-Yan Loo <yan99033@gmail.com>
2022-11-11 16:08:15 -0700

    Fix: x11 port forwarding in the docker container

--- docker/run_docker_container.sh
+++ docker/run_docker_container.sh
@@ -6,6 +6,13 @@ CONT_NAME=VSLAM
 # The root folder 
 PROJECT_ROOT=${PWD%/*}
 
+# X11 port forwarding
+# http://wiki.ros.org/docker/Tutorials/GUI
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 # Create and run the container
 docker run \
 -it \
@@ -15,6 +22,11 @@ docker run \
 --name=$CONT_NAME \
 --entrypoint /bin/bash \
 --mount type=bind,source=$PROJECT_ROOT,target=/home/user/${PROJECT_ROOT##*/} \
+-v /home/bryan/icl_nuim_lr_kt2:/home/user/${PROJECT_ROOT##*/}/datasets:ro \
 -u $(id -u ${USER}):$(id -g ${USER}) \
--w /home/user/VisualSLAMTutorial \
+--volume=$XSOCK:$XSOCK:rw \
+--volume=$XAUTH:$XAUTH:rw \
+--env="XAUTHORITY=${XAUTH}" \
+--env="DISPLAY" \
+-w /home/user/${PROJECT_ROOT##*/} \
 ubuntu/2204:vslamtutorial
