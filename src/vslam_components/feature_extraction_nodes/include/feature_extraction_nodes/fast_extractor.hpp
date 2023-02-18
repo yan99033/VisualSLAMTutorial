fc2f0d7b7797ff6960029c5d787b6652476b3d75
Shing Yan Loo <yan99033@gmail.com>
2023-02-18 09:52:50 -0700

    Chore: add files and dependencies for fast extractor

---
+++ src/vslam_components/feature_extraction_nodes/include/feature_extraction_nodes/fast_extractor.hpp
@@ -0,0 +1,9 @@
+// TODO: Licence
+
+#ifndef DATA_LOADER_NODES__LOAD_FROM_FOLDER_HPP_
+#define DATA_LOADER_NODES__LOAD_FROM_FOLDER_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "vslam_msgs/msg/frame.hpp"
+
+#endif
