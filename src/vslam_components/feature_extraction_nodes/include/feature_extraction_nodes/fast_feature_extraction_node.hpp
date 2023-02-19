d4f5e0f1cda45e4bf17bbd77d67af509a8aee9b5
Shing Yan Loo <yan99033@gmail.com>
2023-02-19 15:47:39 -0700

    Feature: extract ORB features from image

--- src/vslam_components/feature_extraction_nodes/include/feature_extraction_nodes/fast_feature_extraction_node.hpp
+++ src/vslam_components/feature_extraction_nodes/include/feature_extraction_nodes/fast_feature_extraction_node.hpp
@@ -3,10 +3,21 @@
 #ifndef FEATURE_EXTRACTION_NODES__FAST_EXTRACTOR_HPP_
 #define FEATURE_EXTRACTION_NODES__FAST_EXTRACTOR_HPP_
 
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+
 #include "rclcpp/rclcpp.hpp"
 #include "vslam_msgs/msg/frame.hpp"
 
 namespace vslam_components {
+  namespace datastructure {
+    struct OrbFeature {
+      cv::KeyPoint keypoint;
+      cv::Mat descriptor;
+    };
+  }  // namespace datastructure
+
   namespace feature_extraction_nodes {
     class FastFeatureExtractionNode : public rclcpp::Node {
     public:
@@ -14,11 +25,13 @@ namespace vslam_components {
 
     private:
       void frame_callback(const vslam_msgs::msg::Frame::SharedPtr frame_msg) const;
-    
+
       rclcpp::Subscription<vslam_msgs::msg::Frame>::SharedPtr frame_sub_;
       rclcpp::Publisher<vslam_msgs::msg::Frame>::SharedPtr frame_pub_;
-    };
-  }
-}
 
-#endif // FEATURE_EXTRACTION_NODES__FAST_EXTRACTOR_HPP_
+      cv::Ptr<cv::ORB> fast_feature_detector_;
+    };
+  }  // namespace feature_extraction_nodes
+}  // namespace vslam_components
+
+#endif  // FEATURE_EXTRACTION_NODES__FAST_EXTRACTOR_HPP_
--- src/vslam_components/feature_extraction_nodes/src/fast_feature_extraction_node.cpp
+++ src/vslam_components/feature_extraction_nodes/src/fast_feature_extraction_node.cpp
@@ -1,5 +1,7 @@
 #include "feature_extraction_nodes/fast_feature_extraction_node.hpp"
 
+#include <cv_bridge/cv_bridge.h>
+
 using std::placeholders::_1;
 
 namespace vslam_components {
@@ -8,20 +10,42 @@ namespace vslam_components {
 
     FastFeatureExtractionNode::FastFeatureExtractionNode(const rclcpp::NodeOptions &options)
         : Node("fast_feature_extraction_node", options) {
+      // FAST feature detector
+      fast_feature_detector_ = cv::ORB::create(declare_parameter("num_features", 2000));
+
+      // Frame subscriber and publisher
       frame_sub_ = create_subscription<vslam_msgs::msg::Frame>(
-        "in_frame", 10, std::bind(&FastFeatureExtractionNode::frame_callback, this, _1));
+          "in_frame", 10, std::bind(&FastFeatureExtractionNode::frame_callback, this, _1));
       frame_pub_ = create_publisher<vslam_msgs::msg::Frame>("out_frame", 10);
     }
 
-    void FastFeatureExtractionNode::frame_callback(const vslam_msgs::msg::Frame::SharedPtr frame_msg) const {
-      RCLCPP_INFO(this->get_logger(), "Getting frame %u\n", frame_msg->id);
+    void FastFeatureExtractionNode::frame_callback(
+        const vslam_msgs::msg::Frame::SharedPtr frame_msg) const {
+      RCLCPP_INFO(this->get_logger(), "FastFeatureExtractionNode: Getting frame %u\n",
+                  frame_msg->id);
+
+      // Get the image in the frame
+      cv_bridge::CvImagePtr cv_ptr
+          = cv_bridge::toCvCopy(frame_msg->image, frame_msg->image.encoding);
+
+      // Extract features in the image
+      std::vector<cv::KeyPoint> keypoints;
+      cv::Mat descriptors;
+      fast_feature_detector_->detectAndCompute(cv_ptr->image, cv::noArray(), keypoints,
+                                               descriptors);
+      for (size_t i = 0; i < keypoints.size(); i++) {
+        datastructure::OrbFeature orb_feature;
+        orb_feature.keypoint = keypoints[i];
+        descriptors.row(i).copyTo(orb_feature.descriptor);
+      }
     }
-  } // namespace vslam_components
-} // namespace feature_extraction_nodes
+  }  // namespace feature_extraction_nodes
+}  // namespace vslam_components
 
 #include "rclcpp_components/register_node_macro.hpp"
 
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be discoverable when its library
 // is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(vslam_components::feature_extraction_nodes::FastFeatureExtractionNode)
+RCLCPP_COMPONENTS_REGISTER_NODE(
+    vslam_components::feature_extraction_nodes::FastFeatureExtractionNode)
