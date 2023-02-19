e37d89eb60b3d29a231bb51f74bbfa63355ba40b
Shing Yan Loo <yan99033@gmail.com>
2023-02-19 11:58:43 -0700

    Chore: remap topics for connecting subscriber to publisher

--- src/vslam_demos/launch/vslam_from_folder.launch.py
+++ src/vslam_demos/launch/vslam_from_folder.launch.py
@@ -12,33 +12,46 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-  # args that can be set from the command line or a default will be used
-  default_config = os.path.join(
-    get_package_share_directory('vslam_demos'),
-    'config',
-    'test_kitti.yaml'
-  )
-  declare_config_file_cmd = DeclareLaunchArgument(
-    'config', default_value=default_config,
-    description='Full path to the ROS2 parameters file to use for all launched nodes'
-  )
-  config = LaunchConfiguration('config')
+    # args that can be set from the command line or a default will be used
+    default_config = os.path.join(
+        get_package_share_directory('vslam_demos'),
+        'config',
+        'test_kitti.yaml'
+    )
+    declare_config_file_cmd = DeclareLaunchArgument(
+        'config', default_value=default_config,
+        description='Full path to the ROS2 parameters file to use for all launched nodes'
+    )
+    config = LaunchConfiguration('config')
 
-  """Generate launch description with multiple components."""
-  container = ComposableNodeContainer(
-    name='vslam_container',
-    namespace='',
-    package='rclcpp_components',
-    executable='component_container',
-    composable_node_descriptions=[
+    """Generate launch description with multiple components."""
+    container = ComposableNodeContainer(
+        name='vslam_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
             ComposableNode(
                 package='data_loader_nodes',
                 plugin='vslam_components::data_loader_nodes::LoadFromFolder',
                 name='camera_node',
                 parameters=[config],
+                remappings=[
+                    ('/out_frame', '/raw_frame'),
+                ],
                 extra_arguments=[{'use_intra_process_comms': True}],),
-    ],
-    output='screen',
-  )
+            ComposableNode(
+                package='feature_extraction_nodes',
+                plugin='vslam_components::feature_extraction_nodes::FastFeatureExtractionNode',
+                name='fast_feature_extraction_node',
+                parameters=[config],
+                remappings=[
+                    ('/in_frame', '/raw_frame'),
+                    ('/out_frame', '/frame_w_features'),
+                ],
+                extra_arguments=[{'use_intra_process_comms': True}],),
+        ],
+        output='screen',
+    )
 
-  return launch.LaunchDescription([declare_config_file_cmd, container])
+    return launch.LaunchDescription([declare_config_file_cmd, container])
