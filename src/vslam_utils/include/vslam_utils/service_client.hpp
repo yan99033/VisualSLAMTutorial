ac99abb7e7906a3ba6f5d1e795da83a667a59eb5
Shing Yan Loo <yan99033@gmail.com>
2023-03-03 06:20:09 -0700

    Chore: add another way to invoke a service

--- src/vslam_utils/include/vslam_utils/service_client.hpp
+++ src/vslam_utils/include/vslam_utils/service_client.hpp
@@ -28,6 +28,35 @@ namespace vslam_utils {
     typename ResponseType::SharedPtr invoke(typename RequestType::SharedPtr& request,
                                             const std::chrono::nanoseconds timeout
                                             = std::chrono::nanoseconds(-1));
+    /**
+     * @brief Invoke the service and block until completed
+     * @param request The request object to call the service using
+     * @param Response A pointer to the service response from the request
+     * @return bool Whether it was successfully called
+     */
+    bool invoke(typename RequestType::SharedPtr& request,
+                typename ResponseType::SharedPtr& response) {
+      while (!client_->wait_for_service(std::chrono::seconds(1))) {
+        if (!rclcpp::ok()) {
+          throw std::runtime_error(service_name_
+                                   + " service client: interrupted while waiting for service");
+        }
+        RCLCPP_INFO(node_->get_logger(), "%s service client: waiting for service to appear...",
+                    service_name_.c_str());
+      }
+
+      RCLCPP_DEBUG(node_->get_logger(), "%s service client: send async request",
+                   service_name_.c_str());
+      auto future_result = client_->async_send_request(request);
+
+      if (callback_group_executor_.spin_until_future_complete(future_result)
+          != rclcpp::FutureReturnCode::SUCCESS) {
+        return false;
+      }
+
+      response = future_result.get();
+      return response.get();
+    }
 
     /**
      * @brief Block until a service is available or timeout
