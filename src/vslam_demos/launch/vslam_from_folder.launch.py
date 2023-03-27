7a10262e98a86826b9e41facbe2ffed799f294ac
Shing Yan Loo <yan99033@gmail.com>
2023-03-27 07:01:08 -0600

    Chore: launch RViz

--- src/vslam_demos/launch/vslam_from_folder.launch.py
+++ src/vslam_demos/launch/vslam_from_folder.launch.py
@@ -6,23 +6,23 @@ import launch
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     # args that can be set from the command line or a default will be used
-    default_config = os.path.join(
+    default_params = os.path.join(
         get_package_share_directory('vslam_demos'),
-        'config',
+        'params',
         'test_kitti.yaml'
     )
-    declare_config_file_cmd = DeclareLaunchArgument(
-        'config', default_value=default_config,
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params', default_value=default_params,
         description='Full path to the ROS2 parameters file to use for all launched nodes'
     )
-    config = LaunchConfiguration('config')
+    params = LaunchConfiguration('params')
 
     """Generate launch description with multiple components."""
     vslam_preprocessing_container = ComposableNodeContainer(
@@ -35,7 +35,7 @@ def generate_launch_description():
                 package='data_loader_nodes',
                 plugin='vslam_components::data_loader_nodes::LoadFromFolder',
                 name='camera_node',
-                parameters=[config],
+                parameters=[params],
                 remappings=[
                     ('/out_frame', '/raw_frame'),
                 ],
@@ -44,7 +44,7 @@ def generate_launch_description():
                 package='vslam_nodes',
                 plugin='vslam_components::vslam_nodes::IndirectVSlamNode',
                 name='indirect_vslam_node',
-                parameters=[config],
+                parameters=[params],
                 remappings=[
                     ('/in_frame', '/raw_frame'),
                     ('/out_frame', '/frame_w_features'),
@@ -62,5 +62,19 @@ def generate_launch_description():
         output='screen',
     )
 
+    default_rviz_config = os.path.join(
+        get_package_share_directory('vslam_demos'),
+        'config',
+        'rviz_config.rviz'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', default_rviz_config],
+        output='screen'
+    )
+
     # vslam_frontend_nodes
-    return launch.LaunchDescription([declare_config_file_cmd, vslam_preprocessing_container])
+    return launch.LaunchDescription([declare_params_file_cmd, vslam_preprocessing_container, rviz_node])
