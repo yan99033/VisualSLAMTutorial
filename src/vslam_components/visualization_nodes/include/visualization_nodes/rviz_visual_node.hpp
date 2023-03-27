1a0a7651098c86370495ba9fc5758a9490a6d518
Shing Yan Loo <yan99033@gmail.com>
2023-03-27 07:02:15 -0600

    Feature: visualize the live camera poses

--- src/vslam_components/visualization_nodes/include/visualization_nodes/rviz_visual_node.hpp
+++ src/vslam_components/visualization_nodes/include/visualization_nodes/rviz_visual_node.hpp
@@ -3,28 +3,33 @@
 #ifndef VISUALIZATION_NODES__RVIZ_VISUAL_NODE_HPP_
 #define VISUALIZATION_NODES__RVIZ_VISUAL_NODE_HPP_
 
-#include "vslam_msgs/msg/frame.hpp"
-
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 
+#include <Eigen/Dense>
+
 #include "rclcpp/rclcpp.hpp"
 #include "visualization_msgs/msg/marker.hpp"
+#include "vslam_msgs/msg/frame.hpp"
 
 namespace vslam_components {
-namespace visualization_nodes {
-class RvizVisualNode : public rclcpp::Node {
-public:
-  explicit RvizVisualNode(const rclcpp::NodeOptions &options);
+  namespace visualization_nodes {
+    class RvizVisualNode : public rclcpp::Node {
+    public:
+      explicit RvizVisualNode(const rclcpp::NodeOptions &options);
 
-private:
-  void frame_callback(vslam_msgs::msg::Frame::UniquePtr frame_msg);
+    private:
+      void frame_callback(vslam_msgs::msg::Frame::UniquePtr frame_msg);
 
-  rclcpp::Subscription<vslam_msgs::msg::Frame>::SharedPtr frame_sub_;
-  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr
-      live_frame_publisher_;
-};
-} // namespace visualization_nodes
-} // namespace vslam_components
+      rclcpp::Subscription<vslam_msgs::msg::Frame>::SharedPtr frame_sub_;
 
-#endif // VISUALIZATION_NODES__RVIZ_VISUAL_NODE_HPP_
+      static constexpr int live_pose_marker_id_{0};
+
+      rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr live_frame_publisher_;
+
+      static const Eigen::Matrix3d cam_axes_transform_;
+    };
+  }  // namespace visualization_nodes
+}  // namespace vslam_components
+
+#endif  // VISUALIZATION_NODES__RVIZ_VISUAL_NODE_HPP_
--- src/vslam_components/visualization_nodes/src/rviz_visual_node.cpp
+++ src/vslam_components/visualization_nodes/src/rviz_visual_node.cpp
@@ -1,112 +1,142 @@
 #include "visualization_nodes/rviz_visual_node.hpp"
 
+#include <tf2_eigen/tf2_eigen.hpp>
+
+#include "geometry_msgs/msg/pose.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
 using std::placeholders::_1;
 
 namespace {
-int encoding2mat_type(const std::string &encoding) {
-  if (encoding == "mono8") {
-    return CV_8UC1;
-  } else if (encoding == "bgr8") {
-    return CV_8UC3;
-  } else if (encoding == "mono16") {
-    return CV_16SC1;
-  } else if (encoding == "rgba8") {
-    return CV_8UC4;
+  int encoding2mat_type(const std::string& encoding) {
+    if (encoding == "mono8") {
+      return CV_8UC1;
+    } else if (encoding == "bgr8") {
+      return CV_8UC3;
+    } else if (encoding == "mono16") {
+      return CV_16SC1;
+    } else if (encoding == "rgba8") {
+      return CV_8UC4;
+    }
+    throw std::runtime_error("Unsupported mat type");
   }
-  throw std::runtime_error("Unsupported mat type");
-}
 
-// convert coords
-// # Convert from left-hand rule to right-hand rule coordinates system
-// (visualization) # Right-hand rule: x forward, y left, z up # Left-hand rule:
-// z forward, x right, y down convert_coords = True cam_axes_transform =
-// np.array([[0.0 , 0.0 , 1.0],
-//                                [-1.0, 0.0 , 0.0],
-//                                [0.0 , -1.0, 0.0]]).astype(np.float)
-//
-// rot_mat_c = cam_axes_transform @ rot_mat @ np.linalg.inv(cam_axes_transform)
-// trans_c = cam_axes_transform @ trans
+  void calculateLivePoseMarkers(const geometry_msgs::msg::Pose& pose, visualization_msgs::msg::Marker& pose_marker) {
+    // Convert the pose to an Eigen matrix
+    Eigen::Isometry3d eigen_transform;
+    tf2::fromMsg(pose, eigen_transform);
 
-// Create line markers that represent the camera pose (example code)
-// auto marker = visualization_msgs::msg::Marker();
-// marker.header.frame_id = "base_link";
-// marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
-// marker.action = visualization_msgs::msg::Marker::ADD;
-// marker.pose.orientation.w = 1.0;
-// marker.scale.x = 0.1;
-// marker.color.r = 1.0;
-// marker.color.a = 1.0;
+    // Calculate the preset vertices
+    constexpr double s = 1.0;
+    constexpr double fx = 340.0;
+    constexpr double fy = 350.0;
+    constexpr double cx = 300.0;
+    constexpr double cy = 200.0;
+    constexpr double w = 600.0;
+    constexpr double h = 400.0;
+    constexpr double p0[] = {0.0, 0.0, 0.0, 1.0};
+    constexpr double p1[] = {s * (0 - cx) / fx, s * (0 - cy) / fy, s, 1.0};
+    constexpr double p2[] = {s * (0 - cx) / fx, s * (h - 1 - cy) / fy, s, 1.0};
+    constexpr double p3[] = {s * (w - 1 - cx) / fx, s * (h - 1 - cy) / fy, s, 1.0};
+    constexpr double p4[] = {s * (w - 1 - cx) / fx, s * (0 - cy) / fy, s, 1.0};
 
-// auto p1 = geometry_msgs::msg::Point();
-// p1.x = 0;
-// p1.y = 0;
-// p1.z = 0;
+    constexpr int num_vertices = 16;
+    constexpr int num_dims = 4;
+    Eigen::MatrixXd marker_vertices(num_vertices, num_dims);
+    marker_vertices << p0[0], p0[1], p0[2], p0[3],  //
+        p1[0], p1[1], p1[2], p1[3],                 //
 
-// auto p2 = geometry_msgs::msg::Point();
-// p2.x = 1;
-// p2.y = 1;
-// p2.z = 1;
+        p0[0], p0[1], p0[2], p0[3],  //
+        p2[0], p2[1], p2[2], p2[3],  //
 
-// marker.points.push_back(p1);
-// marker.points.push_back(p2);
+        p0[0], p0[1], p0[2], p0[3],  //
+        p3[0], p3[1], p3[2], p3[3],  //
 
-// point pairs needed for drawing the camera frustum
-// glVertex3f(0,0,0);
-// glVertex3f(0.05*(0-cx)/fx,0.05*(0-cy)/fy,0.05);
-// glVertex3f(0,0,0);
-// glVertex3f(0.05*(0-cx)/fx,0.05*(height-1-cy)/fy,0.05);
-// glVertex3f(0,0,0);
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(height-1-cy)/fy,0.05);
-// glVertex3f(0,0,0);
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(0-cy)/fy,0.05);
+        p0[0], p0[1], p0[2], p0[3],  //
+        p4[0], p4[1], p4[2], p4[3],  //
 
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(0-cy)/fy,0.05);
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(height-1-cy)/fy,0.05);
+        p4[0], p4[1], p4[2], p4[3],  //
+        p3[0], p3[1], p3[2], p3[3],  //
 
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(height-1-cy)/fy,0.05);
-// glVertex3f(0.05*(0-cx)/fx,0.05*(height-1-cy)/fy,0.05);
+        p3[0], p3[1], p3[2], p3[3],  //
+        p2[0], p2[1], p2[2], p2[3],  //
 
-// glVertex3f(0.05*(0-cx)/fx,0.05*(height-1-cy)/fy,0.05);
-// glVertex3f(0.05*(0-cx)/fx,0.05*(0-cy)/fy,0.05);
+        p2[0], p2[1], p2[2], p2[3],  //
+        p1[0], p1[1], p1[2], p1[3],  //
 
-// glVertex3f(0.05*(0-cx)/fx,0.05*(0-cy)/fy,0.05);
-// glVertex3f(0.05*(width-1-cx)/fx,0.05*(0-cy)/fy,0.05);
+        p1[0], p1[1], p1[2], p1[3],  //
+        p4[0], p4[1], p4[2], p4[3];  //
 
-} // namespace
+    const auto vertices_transformed = eigen_transform.matrix() * marker_vertices.transpose();
+
+    auto marker = visualization_msgs::msg::Marker();
+
+    // Add vertices to the marker msg
+    pose_marker.header.frame_id = "map";
+    pose_marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    pose_marker.action = visualization_msgs::msg::Marker::ADD;
+    pose_marker.scale.x = 0.1;
+    pose_marker.color.g = 1.0;
+    pose_marker.color.a = 1.0;
+
+    for (int i = 0; i < num_vertices; i++) {
+      auto pt = geometry_msgs::msg::Point();
+      pt.x = vertices_transformed.col(i)(0);
+      pt.y = vertices_transformed.col(i)(1);
+      pt.z = vertices_transformed.col(i)(2);
+
+      pose_marker.points.push_back(pt);
+    }
+  }
+
+  // convert coords
+  // # Convert from left-hand rule to right-hand rule coordinates system
+  // (visualization) # Right-hand rule: x forward, y left, z up # Left-hand rule:
+  // z forward, x right, y down convert_coords = True cam_axes_transform =
+  // np.array([[0.0 , 0.0 , 1.0],
+  //                                [-1.0, 0.0 , 0.0],
+  //                                [0.0 , -1.0, 0.0]]).astype(np.float)
+  //
+  // rot_mat_c = cam_axes_transform @ rot_mat @ np.linalg.inv(cam_axes_transform)
+  // trans_c = cam_axes_transform @ trans
+
+}  // namespace
 
 namespace vslam_components {
 
-namespace visualization_nodes {
+  namespace visualization_nodes {
 
-RvizVisualNode::RvizVisualNode(const rclcpp::NodeOptions &options)
-    : Node("rviz_visual_node", options) {
-  // Frame subscriber and publisher
-  frame_sub_ = create_subscription<vslam_msgs::msg::Frame>(
-      "in_frame", 10, std::bind(&RvizVisualNode::frame_callback, this, _1));
-  live_frame_publisher_ =
-      create_publisher<visualization_msgs::msg::Marker>("live_frame_marker", 1);
+    const Eigen::Matrix3d RvizVisualNode::cam_axes_transform_ =  (Eigen::Matrix3d() << 0.0 , 0.0 , 1.0, -1.0, 0.0, 0.0, 0.0, -1.0, 0.0).finished(); 
 
-  // TODO:
-  // set frame id
-  // create a constexpr for the left-hand to right-hand rule coordinate
-  //   conversion matrix
-}
+    RvizVisualNode::RvizVisualNode(const rclcpp::NodeOptions& options) : Node("rviz_visual_node", options) {
+      // Frame subscriber and publisher
+      frame_sub_ = create_subscription<vslam_msgs::msg::Frame>("in_frame", 10,
+                                                               std::bind(&RvizVisualNode::frame_callback, this, _1));
+      live_frame_publisher_ = create_publisher<visualization_msgs::msg::Marker>("live_frame_marker", 1);
 
-void RvizVisualNode::frame_callback(
-    vslam_msgs::msg::Frame::UniquePtr frame_msg) {
-  RCLCPP_INFO(this->get_logger(), "Getting frame %u", frame_msg->id);
+      // TODO:
+      // set frame id
+      // create a constexpr for the left-hand to right-hand rule coordinate
+      //   conversion matrix
+    }
 
-  // TODO:
-  // 1. transform to the right-hand rule coordinate system
-  // 2. Publish the frame marker
-}
-} // namespace visualization_nodes
-} // namespace vslam_components
+    void RvizVisualNode::frame_callback(vslam_msgs::msg::Frame::UniquePtr frame_msg) {
+      RCLCPP_INFO(this->get_logger(), "Getting frame %u", frame_msg->id);
+
+      // TODO:
+      // 1. transform to the right-hand rule coordinate system
+      // 2. Publish the frame marker
+      visualization_msgs::msg::Marker pose_marker;
+      calculateLivePoseMarkers(frame_msg->pose, pose_marker);
+      pose_marker.id = live_pose_marker_id_;
+      live_frame_publisher_->publish(pose_marker);
+    }
+  }  // namespace visualization_nodes
+}  // namespace vslam_components
 
 #include "rclcpp_components/register_node_macro.hpp"
 
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be
 // discoverable when its library is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(
-    vslam_components::visualization_nodes::RvizVisualNode)
+RCLCPP_COMPONENTS_REGISTER_NODE(vslam_components::visualization_nodes::RvizVisualNode)
