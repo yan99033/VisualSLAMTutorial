553e9a467a9003091e85b14c8f8f7b0efea358ac
Shing Yan Loo <yan99033@gmail.com>
2023-03-28 05:51:55 -0600

    Fix: relative transformation in camera tracking

--- src/vslam_plugins/camera_tracker_plugins/src/indirect.cpp
+++ src/vslam_plugins/camera_tracker_plugins/src/indirect.cpp
@@ -6,8 +6,7 @@
 
 namespace {
   using cvPoint2dVec = std::vector<cv::Point2d>;
-  std::pair<cvPoint2dVec, cvPoint2dVec> getCorrespondences(
-      const vslam_datastructure::MatchedPoints& matched_points) {
+  std::pair<cvPoint2dVec, cvPoint2dVec> getCorrespondences(const vslam_datastructure::MatchedPoints& matched_points) {
     cvPoint2dVec cv_points1;
     cvPoint2dVec cv_points2;
     for (const auto& match : matched_points) {
@@ -32,16 +31,14 @@ namespace {
 namespace vslam_camera_tracker_plugins {
   void Indirect::initialize(const cv::Mat& K) { this->K = K; }
 
-  Sophus::SE3d Indirect::track_camera_2d2d(
-      const vslam_datastructure::MatchedPoints& matched_points) {
+  Sophus::SE3d Indirect::track_camera_2d2d(const vslam_datastructure::MatchedPoints& matched_points) {
     const auto [cv_points1, cv_points2] = getCorrespondences(matched_points);
 
     cv::Mat inlier_mask;
     cv::Mat R;
     cv::Mat t;
-    cv::Mat E
-        = cv::findEssentialMat(cv_points2, cv_points1, K, cv::RANSAC, 0.999, 1.0, inlier_mask);
-    const int num_inliers = cv::recoverPose(E, cv_points2, cv_points1, K, R, t, inlier_mask);
+    cv::Mat E = cv::findEssentialMat(cv_points1, cv_points2, K, cv::RANSAC, 0.999, 1.0, inlier_mask);
+    const int num_inliers = cv::recoverPose(E, cv_points1, cv_points2, K, R, t, inlier_mask);
 
     return cv2Sophus(R, t);
   }
@@ -50,5 +47,4 @@ namespace vslam_camera_tracker_plugins {
 
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(vslam_camera_tracker_plugins::Indirect,
-                       vslam_camera_tracker_base::CameraTracker)
+PLUGINLIB_EXPORT_CLASS(vslam_camera_tracker_plugins::Indirect, vslam_camera_tracker_base::CameraTracker)
