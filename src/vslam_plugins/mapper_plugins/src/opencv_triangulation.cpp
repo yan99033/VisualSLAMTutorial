0aad7eea8b1c81b842443067a07b967c373d16bc
Shing Yan Loo <yan99033@gmail.com>
2023-04-02 19:12:04 -0600

    Fix: triangulation errors

--- src/vslam_plugins/mapper_plugins/src/opencv_triangulation.cpp
+++ src/vslam_plugins/mapper_plugins/src/opencv_triangulation.cpp
@@ -3,26 +3,52 @@
 #include <cmath>
 #include <iostream>
 
+using PointIdx = std::vector<size_t>;
+
 namespace {
-  using cvPoint2dVec = std::vector<cv::Point2d>;
-  using PointIdx = std::vector<size_t>;
-  std::tuple<cvPoint2dVec, cvPoint2dVec, PointIdx> getCorrespondences(
-      const vslam_datastructure::MatchedPoints& matched_points) {
-    cvPoint2dVec cv_points1;
-    cvPoint2dVec cv_points2;
+  std::tuple<cv::Mat, cv::Mat, PointIdx> getCorrespondences(const vslam_datastructure::MatchedPoints& matched_points) {
+    const size_t npts = matched_points.size();
+    cv::Mat points1_mat = cv::Mat(2, npts, CV_64F);
+    cv::Mat points2_mat = cv::Mat(2, npts, CV_64F);
     PointIdx match_idx;
-    size_t i = 0;
-    for (const auto& match : matched_points) {
-      // Check if a map point is already available for the pair
-      if (!match.point1->mappoint.get()) {
-        cv_points1.push_back(match.point1->keypoint.pt);
-        cv_points2.push_back(match.point2->keypoint.pt);
+
+    for (size_t i = 0; i < matched_points.size(); i++) {
+      if (!matched_points[i].point1->mappoint.get()) {
+        points1_mat.at<double>(0, i) = matched_points[i].point1->keypoint.pt.x;
+        points1_mat.at<double>(1, i) = matched_points[i].point1->keypoint.pt.y;
+        points2_mat.at<double>(0, i) = matched_points[i].point2->keypoint.pt.x;
+        points2_mat.at<double>(1, i) = matched_points[i].point2->keypoint.pt.y;
         match_idx.push_back(i);
       }
-      i++;
     }
 
-    return {cv_points1, cv_points2, match_idx};
+    return {points1_mat, points2_mat, match_idx};
+  }
+
+  cv::Mat project_point_3d2d(const cv::Mat& pt, const cv::Mat& K) {
+    cv::Mat pt_projected = pt.clone();
+    pt_projected = K * pt_projected;
+    pt_projected /= pt_projected.at<double>(2, 0);
+
+    return pt_projected.rowRange(0, 2);
+  }
+
+  double find_median_depth(const cv::Mat& mat) {
+    // process the non-zero values only
+    cv::Mat mat_cpy = mat.clone();
+    mat_cpy = cv::max(mat_cpy, cv::Scalar(0));
+    const int nonzeros_count = cv::countNonZero(mat_cpy);
+    const int half_size = nonzeros_count / 2;
+
+    // Copy elements to vector
+    std::vector<double> sorted(mat_cpy.rows * mat_cpy.cols * mat_cpy.channels());
+    assert(mat_cpy.isContinuous());
+    sorted.assign(mat_cpy.ptr<double>(0), mat_cpy.ptr<double>(0) + mat_cpy.total());
+
+    // Calculate the median of the non-zero values
+    std::nth_element(sorted.begin(), sorted.begin() + half_size - 1, sorted.end(), std::greater<double>());
+
+    return sorted.at(half_size);
   }
 }  // namespace
 
@@ -30,7 +56,8 @@ namespace vslam_mapper_plugins {
   void OpenCvTriangulation::initialize(const cv::Mat& K) { K_ = K; }
 
   vslam_datastructure::MapPoints OpenCvTriangulation::map(vslam_datastructure::MatchedPoints& matched_points,
-                                                          const cv::Mat& T_1_w, const cv::Mat& T_2_1) {
+                                                          const cv::Mat& T_1_w, const cv::Mat& T_2_1,
+                                                          const bool normalize_depth) {
     // Get the matched points that don't have a map point
     // e.g., calculate a std::vector<bool> mask to indicate which matched points do not have a 3D point
     const auto [cv_points1, cv_points2, match_idx] = getCorrespondences(matched_points);
@@ -44,15 +71,33 @@ namespace vslam_mapper_plugins {
     cv::Mat cv_points1_3d;
     cv::triangulatePoints(P1, P2, cv_points1, cv_points2, cv_points1_3d);
 
+    // map scale
+    const double pt_scale = [&]() {
+      if (normalize_depth) {
+        cv::Mat normalized_z = cv_points1_3d.row(2) / cv_points1_3d.row(3);
+        return find_median_depth(normalized_z);
+      } else {
+        return 1.0;
+      }
+    }();
+
     // Create map points for the points
     vslam_datastructure::MapPoints new_mps;
-    for (size_t i = 0; i < cv_points1.size(); i++) {
+    for (size_t i = 0; i < cv_points1.cols; i++) {
       // Normalize and transform to the global coordinates
       auto pt_3d = cv_points1_3d.col(i);
       pt_3d /= pt_3d.at<double>(3, 0);
+      pt_3d /= pt_scale;
 
       // Remove points behind the camera
       if (pt_3d.at<double>(2) < 0.0) {
+        continue;
+      }
+
+      // Remove points that have a large re-projection error
+      cv::Mat pt_3d_projected = project_point_3d2d(pt_3d.rowRange(0, 3), K_);
+      constexpr double proj_err_thresh = 8.0;
+      if (cv::norm(pt_3d_projected, cv_points1.col(i)) > proj_err_thresh) {
         continue;
       }
 
@@ -72,7 +117,6 @@ namespace vslam_mapper_plugins {
 
       new_mps.push_back(mp);
     }
-
     return new_mps;
   }
 
