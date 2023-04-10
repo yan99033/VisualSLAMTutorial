295c8ca88c9b17b3fa93256beef631ec31025603
Shing Yan Loo <yan99033@gmail.com>
2023-04-10 07:18:53 -0600

    Feature: add a local ba loop to run local ba when a new keyframe is added

--- src/vslam_plugins/backend_plugins/include/vslam_backend_plugins/indirect.hpp
+++ src/vslam_plugins/backend_plugins/include/vslam_backend_plugins/indirect.hpp
@@ -1,6 +1,10 @@
 #ifndef VSLAM_BACKEND_PLUGINS__INDIRECT_HPP_
 #define VSLAM_BACKEND_PLUGINS__INDIRECT_HPP_
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include "vslam_datastructure/frame.hpp"
 #include "vslam_plugins_base/backend.hpp"
 
@@ -8,6 +12,8 @@ namespace vslam_backend_plugins {
 
   class Indirect : public vslam_backend_base::Backend {
   public:
+    ~Indirect();
+
     void initialize() override;
 
     void add_keyframe(vslam_datastructure::Frame::SharedPtr keyframe) override;
@@ -17,9 +23,20 @@ namespace vslam_backend_plugins {
     vslam_datastructure::Frame::SharedPtr get_current_keyframe() override;
 
   private:
+    // All keyframes (can be looked up using their id)
     std::unordered_map<long unsigned int, vslam_datastructure::Frame::SharedPtr> keyframes_;
 
+    // The keyframe that is closest to the current camera pose
     vslam_datastructure::Frame::SharedPtr current_keyframe_;
+
+    // Local BA
+    std::condition_variable local_ba_condition_;
+    std::atomic_bool run_local_ba_{false};
+    std::mutex local_ba_mutex_;
+    std::thread local_ba_thread_;
+    void local_ba_loop();
+
+    std::atomic_bool exit_thread_{false};
   };
 }  // namespace vslam_backend_plugins
 
--- src/vslam_plugins/backend_plugins/src/indirect.cpp
+++ src/vslam_plugins/backend_plugins/src/indirect.cpp
@@ -1,7 +1,16 @@
 #include "vslam_backend_plugins/indirect.hpp"
 
 namespace vslam_backend_plugins {
-  void Indirect::initialize() {}
+  Indirect::~Indirect() {
+    exit_thread_ = true;
+
+    if (local_ba_thread_.joinable()) {
+      local_ba_condition_.notify_one();
+      local_ba_thread_.join();
+    }
+  }
+
+  void Indirect::initialize() { local_ba_thread_ = std::thread(&Indirect::local_ba_loop, this); }
 
   void Indirect::add_keyframe(vslam_datastructure::Frame::SharedPtr keyframe) {
     assert(keyframe->is_keyframe());
@@ -9,6 +18,12 @@ namespace vslam_backend_plugins {
     keyframes_[keyframe->id()] = keyframe;
 
     current_keyframe_ = keyframe;
+
+    {
+      std::lock_guard<std::mutex> lck(local_ba_mutex_);
+      run_local_ba_ = true;
+      local_ba_condition_.notify_one();
+    }
   }
 
   void Indirect::remove_keyframe(vslam_datastructure::Frame::SharedPtr keyframe) {
@@ -18,6 +33,24 @@ namespace vslam_backend_plugins {
   }
 
   vslam_datastructure::Frame::SharedPtr Indirect::get_current_keyframe() { return current_keyframe_; }
+
+  void Indirect::local_ba_loop() {
+    while (!exit_thread_) {
+      {
+        std::unique_lock<std::mutex> lck(local_ba_mutex_);
+        local_ba_condition_.wait(lck, [this] { return exit_thread_ || run_local_ba_; });
+      }
+
+      if (exit_thread_) {
+        break;
+      }
+
+      std::cout << "run local BA" << std::endl;
+
+      run_local_ba_ = false;
+    }
+    std::cout << "terminated local BA" << std::endl;
+  }
 
 }  // namespace vslam_backend_plugins
 
