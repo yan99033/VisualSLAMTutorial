adaa93b0c756aa0d845548f0b532be4814a211de
Shing Yan Loo <yan99033@gmail.com>
2023-04-23 16:44:03 -0600

    Style: auto-format code

--- src/vslam_components/data_loader_nodes/src/load_from_folder.cpp
+++ src/vslam_components/data_loader_nodes/src/load_from_folder.cpp
@@ -17,93 +17,87 @@
 using namespace std::chrono_literals;
 
 namespace {
-std::vector<std::string> load_files(const std::string &folder,
-                                    std::string ext = ".png") {
-  std::vector<std::string> files;
+  std::vector<std::string> load_files(const std::string &folder, std::string ext = ".png") {
+    std::vector<std::string> files;
 
-  assert(std::filesystem::exists(folder) || !std::filesystem::is_empty(folder));
+    assert(std::filesystem::exists(folder) || !std::filesystem::is_empty(folder));
 
-  for (const auto &f : std::filesystem::directory_iterator(folder)) {
-    if (f.path().extension() == ext) {
-      files.push_back(f.path());
+    for (const auto &f : std::filesystem::directory_iterator(folder)) {
+      if (f.path().extension() == ext) {
+        files.push_back(f.path());
+      }
+    }
+
+    assert(!files.empty());
+
+    std::sort(files.begin(), files.end());
+
+    return files;
+  }
+
+  std::string mat_type2encoding(int mat_type) {
+    switch (mat_type) {
+      case CV_8UC1:
+        return "mono8";
+      case CV_8UC3:
+        return "bgr8";
+      case CV_16SC1:
+        return "mono16";
+      case CV_8UC4:
+        return "rgba8";
+      default:
+        throw std::runtime_error("Unsupported encoding type");
     }
   }
-
-  assert(!files.empty());
-
-  std::sort(files.begin(), files.end());
-
-  return files;
-}
-
-std::string mat_type2encoding(int mat_type) {
-  switch (mat_type) {
-  case CV_8UC1:
-    return "mono8";
-  case CV_8UC3:
-    return "bgr8";
-  case CV_16SC1:
-    return "mono16";
-  case CV_8UC4:
-    return "rgba8";
-  default:
-    throw std::runtime_error("Unsupported encoding type");
-  }
-}
-} // namespace
+}  // namespace
 
 namespace vslam_components {
 
-namespace data_loader_nodes {
+  namespace data_loader_nodes {
 
-LoadFromFolder::LoadFromFolder(const rclcpp::NodeOptions &options)
-    : Node("load_from_folder_node", options), count_(0),
-      files_{load_files(declare_parameter("image_folder", ""))} {
-  frame_pub_ = create_publisher<vslam_msgs::msg::Frame>("out_frame", 10);
+    LoadFromFolder::LoadFromFolder(const rclcpp::NodeOptions &options)
+        : Node("load_from_folder_node", options), count_(0), files_{load_files(declare_parameter("image_folder", ""))} {
+      frame_pub_ = create_publisher<vslam_msgs::msg::Frame>("out_frame", 10);
 
-  // Use a timer to schedule periodic message publishing.
-  auto frame_rate_hz = declare_parameter("frame_rate_hz", 10);
-  assert(frame_rate_hz > 0);
-  timer_ = create_wall_timer(std::chrono::duration<double>(1. / frame_rate_hz),
-                             std::bind(&LoadFromFolder::on_timer, this));
+      // Use a timer to schedule periodic message publishing.
+      auto frame_rate_hz = declare_parameter("frame_rate_hz", 10);
+      assert(frame_rate_hz > 0);
+      timer_ = create_wall_timer(std::chrono::duration<double>(1. / frame_rate_hz),
+                                 std::bind(&LoadFromFolder::on_timer, this));
 
-  RCLCPP_INFO(this->get_logger(), "Going to publish the frames at %llu hz \n",
-              frame_rate_hz);
-  RCLCPP_INFO(this->get_logger(), "Loaded '%lu' files\n", files_.size());
-}
+      RCLCPP_INFO(this->get_logger(), "Going to publish the frames at %llu hz \n", frame_rate_hz);
+      RCLCPP_INFO(this->get_logger(), "Loaded '%lu' files\n", files_.size());
+    }
 
-void LoadFromFolder::on_timer() {
-  // Load image
-  cv::Mat image =
-      cv::imread(files_.at(count_ % files_.size()), cv::IMREAD_COLOR);
-  sensor_msgs::msg::Image im_msg;
-  im_msg.height = image.rows;
-  im_msg.width = image.cols;
-  im_msg.encoding = mat_type2encoding(image.type());
-  im_msg.is_bigendian = false;
-  im_msg.step = static_cast<sensor_msgs::msg::Image::_step_type>(image.step);
-  im_msg.data.assign(image.datastart, image.dataend);
+    void LoadFromFolder::on_timer() {
+      // Load image
+      cv::Mat image = cv::imread(files_.at(count_ % files_.size()), cv::IMREAD_COLOR);
+      sensor_msgs::msg::Image im_msg;
+      im_msg.height = image.rows;
+      im_msg.width = image.cols;
+      im_msg.encoding = mat_type2encoding(image.type());
+      im_msg.is_bigendian = false;
+      im_msg.step = static_cast<sensor_msgs::msg::Image::_step_type>(image.step);
+      im_msg.data.assign(image.datastart, image.dataend);
 
-  // Create a frame
-  auto msg = std::make_unique<vslam_msgs::msg::Frame>();
-  msg->id = ++count_;
-  RCLCPP_INFO(this->get_logger(), "Publishing frame: %u / %lu", msg->id,
-              files_.size());
-  msg->image = im_msg;
-  msg->header.stamp = now();
+      // Create a frame
+      auto msg = std::make_unique<vslam_msgs::msg::Frame>();
+      msg->id = ++count_;
+      RCLCPP_INFO(this->get_logger(), "Publishing frame: %u / %lu", msg->id, files_.size());
+      msg->image = im_msg;
+      msg->header.stamp = now();
 
-  // Put the message into a queue to be processed by the middleware.
-  // This call is non-blocking.
-  frame_pub_->publish(std::move(msg));
-}
-} // namespace data_loader_nodes
+      // Put the message into a queue to be processed by the middleware.
+      // This call is non-blocking.
+      frame_pub_->publish(std::move(msg));
+    }
+  }  // namespace data_loader_nodes
 
-} // namespace vslam_components
+}  // namespace vslam_components
 
 #include "rclcpp_components/register_node_macro.hpp"
 
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be discoverable
 // when its library is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(
-    vslam_components::data_loader_nodes::LoadFromFolder)
+RCLCPP_COMPONENTS_REGISTER_NODE(vslam_components::data_loader_nodes::LoadFromFolder)
