13dd90bc3e93f625bc8f248735671c0c039a1a43
Shing Yan Loo <yan99033@gmail.com>
2023-05-06 11:24:43 -0600

    Fix: only add unique frames to the queue

--- src/vslam_datastructure/include/vslam_datastructure/signal_queue.hpp
+++ src/vslam_datastructure/include/vslam_datastructure/signal_queue.hpp
@@ -13,14 +13,14 @@ namespace vslam_datastructure {
   public:
     using SharedPtr = std::shared_ptr<SignalQueue>;
 
-    void send(T &&msg) {
+    void send(T&& msg) {
       std::lock_guard<std::mutex> lck(mutex_);
       queue_.push_back(std::move(msg));
 
       condition_.notify_one();
     }
 
-    void send_front(T &&msg) {
+    void send_front(T&& msg) {
       std::lock_guard<std::mutex> lck(mutex_);
       queue_.push_front(std::move(msg));
 
@@ -60,9 +60,51 @@ namespace vslam_datastructure {
 
     std::atomic_bool exit_{false};
   };
-
-  using FrameMsgQueue = SignalQueue<vslam_msgs::msg::Frame>;
   using FrameIdQueue = SignalQueue<long unsigned int>;
+
+  class FrameMsgQueue {
+  public:
+    using SharedPtr = std::shared_ptr<FrameMsgQueue>;
+
+    void send(vslam_msgs::msg::Frame&& msg) {
+      std::lock_guard<std::mutex> lck(mutex_);
+      map_[msg.id] = std::move(msg);
+      condition_.notify_one();
+    }
+
+    void stop() {
+      // Unblock the condition variable
+      exit_ = true;
+      condition_.notify_one();
+    }
+
+    vslam_msgs::msg::Frame receive() {
+      std::unique_lock<std::mutex> lck(mutex_);
+      condition_.wait(lck, [this] { return !map_.empty() || exit_; });
+
+      if (exit_) {
+        map_.clear();
+        return vslam_msgs::msg::Frame();
+      }
+
+      if (map_.empty()) {
+        return vslam_msgs::msg::Frame();
+      }
+
+      // Remove the last element in the map
+      vslam_msgs::msg::Frame msg = std::move(std::prev(map_.end())->second);
+      map_.erase(std::prev(map_.end()));
+
+      return msg;
+    }
+
+  private:
+    std::map<long unsigned int, vslam_msgs::msg::Frame, std::less<long unsigned int>> map_;
+    std::condition_variable condition_;
+    std::mutex mutex_;
+
+    std::atomic_bool exit_{false};
+  };
 
 }  // namespace vslam_datastructure
 #endif  // VSLAM_DATASTRUCTURE__SIGNAL_QUEUE_HPP_
