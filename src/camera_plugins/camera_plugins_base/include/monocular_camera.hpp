6141da5244f3ea488ec4a486b0c8a86e68e71131
Shing Yan Loo <yan99033@gmail.com>
2023-05-18 18:27:15 -0600

    Chore: create a package for base camera plugins

---
+++ src/camera_plugins/camera_plugins_base/include/monocular_camera.hpp
@@ -0,0 +1,8 @@
+#ifndef CAMERA_PLUGINS_BASE__MONOCULAR_HPP_
+#define CAMERA_PLUGINS_BASE__MONOCULAR_HPP_
+
+namespace camera_plugins_base {
+  class MonocularCamera {};
+}  // namespace camera_plugins_base
+
+#endif  // CAMERA_PLUGINS_BASE__MONOCULAR_HPP_
