934cfe41dc6fdfa0ee932e27c9d144596aa37e46
Shing Yan Loo <yan99033@gmail.com>
2023-06-11 18:27:27 -0600

    Cleanup: remove comments

--- src/vslam_components/vslam_nodes/src/indirect_vslam_node.cpp
+++ src/vslam_components/vslam_nodes/src/indirect_vslam_node.cpp
@@ -583,7 +583,4 @@ int main(int argc, char* argv[]) {
 
 #include "rclcpp_components/register_node_macro.hpp"
 
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be
-// discoverable when its library is being loaded into a running process.
 RCLCPP_COMPONENTS_REGISTER_NODE(vslam_components::vslam_nodes::IndirectVSlamNode)
