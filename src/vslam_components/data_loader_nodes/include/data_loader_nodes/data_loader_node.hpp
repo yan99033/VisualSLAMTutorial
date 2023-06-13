3cce4a46e6a126f8348f77fd1311470361f15f22
Shing-Yan Loo <yan99033@gmail.com>
2023-06-12 20:01:40 -0600

    Chore: prepend licence

--- src/vslam_components/data_loader_nodes/include/data_loader_nodes/data_loader_node.hpp
+++ src/vslam_components/data_loader_nodes/include/data_loader_nodes/data_loader_node.hpp
@@ -1,4 +1,20 @@
-// TODO: Licence
+/**
+ * This file is part of VisualSLAMTutorial
+ *
+ * Copyright (C) 2023  Shing-Yan Loo <yan99033 at gmail dot com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef DATA_LOADER_NODES__DATA_LOADER_NODE_HPP_
 #define DATA_LOADER_NODES__DATA_LOADER_NODE_HPP_
